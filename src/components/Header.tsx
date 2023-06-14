b14b15625a7fd1978cc774a2a0af348a30f0b055
조정훈 <jeongh1021@naver.com>
2023-06-14 23:19:26 +0900

    Nav activeBar수정 완료

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import { Link, useLocation, useMatch } from "react-router-dom";
-import { BsSunFill, BsMoonFill } from "react-icons/bs";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { themeState } from "../atoms";
-import { motion } from "framer-motion";
+import { BsSunFill, BsMoonFill } from "react-icons/bs";
+import NavLink from "./NavLink";
 
 const StyledHeader = styled.div`
   width: 100%;
@@ -13,6 +12,7 @@ const StyledHeader = styled.div`
   align-items: center;
   font-weight: 600;
   font-size: 1.5rem;
+  // border: 1px solid red;
 `;
 
 const LinkContainer = styled.div`
@@ -33,7 +33,7 @@ const IconContainer = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: -3px;
+  margin-top: -15px;
   font-size: 1rem;
   background-color: transparent;
   border: none;
@@ -41,74 +41,21 @@ const IconContainer = styled.button`
   cursor: pointer;
 `;
 
-const Circle = styled(motion.span)`
-  width: 5px;
-  height: 5px;
-  border-radius: 5px;
-  bottom: -5px;
-  border: 1px solid #999;
-`;
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-  },
-  end: {
-    opacity: 1,
-  },
-};
-
-interface NavePrpos {
-  to: string;
-  title: string;
-}
-
 const Header = () => {
   const [isDarkMode, setIsDarkMode] = useRecoilState(themeState);
-  const handelDarkMode = () => {
+  const handleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
-  };
-
-  const location = useLocation();
-  const pathName = location.pathname.slice(1).replaceAll(" ", "");
-
-  console.log(pathName, "pathName");
-
-  const isActivePath = (path: string) => pathName === path;
-  const getLinkStyles = (path: string) => ({
-    color: isActivePath(path) ? (!isDarkMode ? "white" : "#222222") : "#999",
-    fontWeight: isActivePath(path) ? "700" : "500",
-  });
-
-  const NavLink = ({ to, title }: NavePrpos) => {
-    const isActive = useMatch(to);
-    return (
-      <Link style={getLinkStyles(to.replace("/", ""))} to={to}>
-        {title}
-        {isActive && (
-          <Circle
-            variants={circleVariants}
-            initial="start"
-            animate="end"
-            transition={{
-              duration: 0.3,
-            }}
-            layoutId="circle"
-          />
-        )}
-      </Link>
-    );
   };
 
   return (
     <StyledHeader>
       <div></div>
       <LinkContainer>
-        <NavLink to="/" title="POPULAR" />
-        <NavLink to="/comming-soon" title="COMMING SOON" />
-        <NavLink to="/now-playing" title="NOW PLAYING" />
+        <NavLink to="/">POPULAR</NavLink>
+        <NavLink to="/comming-soon">COMMING SOON</NavLink>
+        <NavLink to="/now-playing">NOW PLAYING</NavLink>
       </LinkContainer>
-      <IconContainer onClick={handelDarkMode}>
+      <IconContainer onClick={handleDarkMode}>
         {isDarkMode ? <BsSunFill /> : <BsMoonFill />}
       </IconContainer>
     </StyledHeader>
---
+++ src/components/NavLink.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import { Link, useMatch } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useRecoilValue } from "recoil";
+import { themeState } from "../atoms";
+import { darkTheme, lightTheme } from "../theme";
+
+interface NavLinkProps {
+  to: string;
+  children: React.ReactNode;
+}
+
+const ActiveBar = styled(motion.span)`
+  width: 100%;
+  height: 2px;
+  margin-top: 5px;
+  border-radius: 5px;
+  bottom: -5px;
+  background-color: ${(props) => props.theme.textColor};
+`;
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+  },
+  end: {
+    opacity: 1,
+  },
+};
+
+const NavLink = ({ to, children }: NavLinkProps) => {
+  const isDarkMode = useRecoilValue(themeState);
+  const isActive = useMatch(to);
+
+  return (
+    <Link
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontWeight: isActive ? 700 : 500,
+        color: isActive
+          ? !isDarkMode
+            ? darkTheme.textColor
+            : lightTheme.textColor
+          : "#999",
+      }}
+      to={to}
+    >
+      {children}
+      {isActive && (
+        <ActiveBar
+          variants={circleVariants}
+          initial="start"
+          animate="end"
+          transition={{
+            duration: 0.3,
+          }}
+          layoutId="circle"
+        />
+      )}
+    </Link>
+  );
+};
+
+export default NavLink;
